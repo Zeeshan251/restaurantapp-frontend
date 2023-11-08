3ab0606437567a8623912ef777f1ecdd81506d65
Zeeshan251 <zeeshan202125@gmail.com>
2023-11-08 16:39:04 +0530

    added code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/InitialState";
+import reducer from "./context/Reducer";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+  <Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </Router>,
 
-, document.getElementById("root"));
+  document.getElementById("root")
+);
---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import FI1 from "../img/fi1.png";
+
+export const heroData = [
+    {id : 1, name : 'Icecream', decp : 'chocolate & Vanilla', price : '5.25', imageSrc : I1},
+
+
+    {id : 2, name : 'Strawberries', decp : 'Fresh Strawberries', price : '10.25', imageSrc : F1},
+
+    {id : 3, name : 'Ckicken Kebab', decp : 'Mixed Chicken Kebab ', price : '8.50', imageSrc : C3},
+
+
+    {id : 4, name : 'Fried Fish', decp : 'Mixed Fried Fish', price : '5.25', imageSrc : FI1}
+]
