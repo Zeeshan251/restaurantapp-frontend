18854869a6c0a48cc4e205313eb73edd8e0e957a
Zeeshan251 <zeeshan202125@gmail.com>
2023-11-26 18:19:00 +0530

    added code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
   };
 
   return (
-    <header className=" fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop and tablet */}
 
       <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -67,16 +67,24 @@ const Header = () => {
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8 "
           >
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            onClick={()=> setIsMenu(false)}
+            >
               Home
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            onClick={()=> setIsMenu(false)}
+            >
               Menu
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            onClick={()=> setIsMenu(false)}
+            >
               About Us
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            onClick={()=> setIsMenu(false)}
+            >
               Service
             </li>
           </motion.ul>
@@ -105,7 +113,9 @@ const Header = () => {
               >
                 {user && user.email === "zeeshan202125@gmail.com" && (
                   <Link to={"/createItem"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                    onClick={()=> setIsMenu(false)}
+                    >
                       New Item <MdAdd />
                     </p>
                   </Link>
@@ -156,7 +166,8 @@ const Header = () => {
             >
               {user && user.email === "zeeshan202125@gmail.com" && (
                 <Link to={"/createItem"}>
-                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={()=> setIsMenu(false)}>
                     New Item <MdAdd />
                   </p>
                 </Link>
--- src/utils/data.js
+++ src/utils/data.js
@@ -4,13 +4,80 @@ import C3 from "../img/c3.png";
 import FI1 from "../img/fi1.png";
 
 export const heroData = [
-    {id : 1, name : 'Icecream', decp : 'chocolate & Vanilla', price : '5.25', imageSrc : I1},
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "chocolate & Vanilla",
+    price: "5.25",
+    imageSrc: I1,
+  },
+
+  {
+    id: 2,
+    name: "Strawberries",
+    decp: "Fresh Strawberries",
+    price: "10.25",
+    imageSrc: F1,
+  },
+
+  {
+    id: 3,
+    name: "Ckicken Kebab",
+    decp: "Mixed Chicken Kebab ",
+    price: "8.50",
+    imageSrc: C3,
+  },
+
+  {
+    id: 4,
+    name: "Fried Fish",
+    decp: "Mixed Fried Fish",
+    price: "5.25",
+    imageSrc: FI1,
+  },
+];
 
 
-    {id : 2, name : 'Strawberries', decp : 'Fresh Strawberries', price : '10.25', imageSrc : F1},
+export const categories = [
 
-    {id : 3, name : 'Ckicken Kebab', decp : 'Mixed Chicken Kebab ', price : '8.50', imageSrc : C3},
+    {
+        id : 1,
+        name : "Chicken",
+        urlParamName : "chicken"
+    },
 
+    {
+        id : 2,
+        name : "Curry",
+        urlParamName : "curry"
+    },
 
-    {id : 4, name : 'Fried Fish', decp : 'Mixed Fried Fish', price : '5.25', imageSrc : FI1}
+    {
+        id : 3,
+        name : "Rice",
+        urlParamName : "rice"
+    },
+
+    {
+        id : 4,
+        name : "Fish",
+        urlParamName : "fish"
+    },
+
+    {
+        id : 5,
+        name : "Fruits",
+        urlParamName : "fruits"
+    },
+    {
+        id : 6,
+        name : "Icecreams",
+        urlParamName : "icecreams"
+    },
+    {
+        id : 7,
+        name : "Soft Drinks",
+        urlParamName : "drinks"
+    },
+
 ]
